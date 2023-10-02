fa324a684ad228c1252002a26073399a69ee5b62
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2023-10-02 07:05:00 +0300

    github login

--- src/routes/auth/components/OAuthProviders.tsx
+++ src/routes/auth/components/OAuthProviders.tsx
@@ -7,10 +7,25 @@ interface OAuthprovidersProps {
 
 export function OAuthproviders({}:OAuthprovidersProps){
 return (
- <div className='w-full h-full flex items-center justify-center'>
-    <Link href="/api/auth/oauth/github" className="btn btn-outline" prefetch="never">
-        <GithubIcon className="w-6 h-6"/>
-    </Link>
- </div>
+  <div className="w-full h-full flex flex-col items-center justify-center gap-5">
+    
+    <div className="w-full flex items-center justify-center">
+        <span className="w-full border-t" />
+        <span className="bg-background px-2 text-muted-foreground min-w-fit">
+          Or continue with
+        </span>
+        <span className="w-full border-t" />
+    </div>
+
+    <div className="w-full h-full flex  items-center justify-center gap-2">
+      <Link
+        href="/api/auth/oauth/github"
+        className="btn btn-outline w-[70%] md:w-[40%] lg:w-[30%]"
+        prefetch="never"
+      >
+        Login with <GithubIcon className="w-6 h-6" />
+      </Link>
+    </div>
+  </div>
 );
 }
