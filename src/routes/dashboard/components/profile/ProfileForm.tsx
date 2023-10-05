7d8238a7e439f56f96d851f8be17d36aee2189bd
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2023-10-05 20:55:52 +0300

    format code

--- src/routes/dashboard/components/profile/ProfileForm.tsx
+++ src/routes/dashboard/components/profile/ProfileForm.tsx
@@ -1,4 +1,4 @@
-import {  useState } from "react";
+import { useState } from "react";
 import { Edit, Loader } from "lucide-react";
 import { useFormHook } from "@/components/form/useForm";
 import { TUserProfileInputType } from "./api";
@@ -14,180 +14,178 @@ import { TheStringListInput } from "@/components/form/inputs/StringListInput";
 import { Button } from "@/components/shadcn/ui/button";
 
 interface ProfileFormProps {
-  user:Partial<TUserProfileInputType>&{avatar?:string,userId:string}
-  updating?:boolean
+  user: Partial<TUserProfileInputType> & { avatar?: string; userId: string };
+  updating?: boolean;
 }
 
-
-
-export function ProfileForm({user,updating}:ProfileFormProps){
-
-const mutation = useSSM<any,TUserProfileInputType>(async(ctx,vars)=>{
-  try {
+export function ProfileForm({ user, updating }: ProfileFormProps) {
+  const mutation = useSSM<any, TUserProfileInputType>(async (ctx, vars) => {
+    try {
       await prisma.user.update({
         where: {
-          id:user.userId,
+          id: user.userId,
         },
         data: {
           ...vars,
         },
       });
-  } catch (error) {
-    throw error
-  }
-
-})
-
-
-const { handleChange, input, setError, setInput, validateInputs } =
-  useFormHook<TUserProfileInputType>({
-    initialValues: {
-      about_me: user.about_me ?? "",
-      email: user.email ?? "",
-      github_username: user.github_username ?? "",
-      linkedin_username: user.linkedin_username ?? "",
-      name: user.name ?? "",
-      avatar: user?.avatar ?? "",
-      country: user.country ?? "",
-      city: user.city ?? "",
-      phone: user.phone ?? "",
-      skills: user.skills ?? "",
-    },
+    } catch (error) {
+      throw error;
+    }
   });
 
+  const { handleChange, input, setError, setInput, validateInputs } =
+    useFormHook<TUserProfileInputType>({
+      initialValues: {
+        about_me: user.about_me ?? "",
+        email: user.email ?? "",
+        github_username: user.github_username ?? "",
+        linkedin_username: user.linkedin_username ?? "",
+        name: user.name ?? "",
+        avatar: user?.avatar ?? "",
+        country: user.country ?? "",
+        city: user.city ?? "",
+        phone: user.phone ?? "",
+        skills: user.skills ?? "",
+      },
+    });
 
-const [editing,setEditing] = useState(!updating)
+  const [editing, setEditing] = useState(!updating);
 
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    e.stopPropagation();
+    mutation
+      .mutateAsync(input)
+      .then(() => toast("Profile created successfully", { type: "success" }))
+      .catch((error) =>
+        toast(error.message, { type: "error", autoClose: false })
+      );
+  }
 
-function handleSubmit(e:React.FormEvent<HTMLFormElement>){
-e.preventDefault()
-e.stopPropagation()
-mutation
-  .mutateAsync(input)
-  .then(() => toast("Profile created successfully", { type: "success" }))
-  .catch((error) => toast(error.message, { type: "error" ,autoClose: false}));
+  const text_fields: Array<keyof TUserProfileInputType> = [
+    "email",
+    "name",
+    "github_username",
+    "linkedin_username",
+  ];
+  const text_area_fields: Array<keyof TUserProfileInputType> = ["about_me"];
 
-}
-
-const text_fields:Array<keyof TUserProfileInputType>=["email","name","github_username","linkedin_username"]
-const text_area_fields:Array<keyof TUserProfileInputType>=["about_me"]
-
-
-return (
-  <div className="flex h-full w-full flex-col items-center justify-center">
-    <form
-      onSubmit={handleSubmit}
-      className="m-1 flex h-full w-full flex-col items-center justify-center"
-    >
-      <div className="flex w-full justify-end px-5">
-        <Edit
-          className={editing ? "h-6 w-6 text-accent" : "h-6 w-6"}
-          onClick={() => setEditing(!editing)}
-        />
-      </div>
-      <div
-        className="flex  justify-start items-center md:items-start h-full w-full flex-col gap-10
-       rounded-lg bg-secondary/5 p-2 md:flex-row"
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <form
+        onSubmit={handleSubmit}
+        className="m-1 flex h-full w-full flex-col items-center justify-center"
       >
-        {/* avatar */}
-        <ThePicUrlInput
-          container_classname="md:w-fit"
-          img_url={input.avatar}
-          editing={editing}
-          setInputImage={(url) =>
-            setInput((prev) => {
-              return {
-                ...prev,
-                image_url: url ?? "",
-              };
-            })
-          }
-        />
-
-        <div className="flex h-full w-full flex-col items-center gap-5">
-          {/* text fields */}
-          <div className="flex w-full flex-col items-center justify-center gap-2 lg:flex-row">
-            {text_fields.map((field) => {
-              return (
-                <TheTextInput
-                  key={field}
-                  field_key={field}
-                  value={input[field]}
-                  // input={input}
-                  field_name={field}
-                  className="input input-bordered input-sm w-full  "
-                  label_classname="font-thin"
-                  onChange={handleChange}
-                  editing={editing}
-                />
-              );
-            })}
-          </div>
-          {/* text area fields */}
-          <div className="flex w-full flex-col items-center justify-center gap-1 ">
-            {text_area_fields.map((field) => {
-              return (
-                <TheTextAreaInput
-                  field_key={field}
-                  key={field}
-                  value={input[field]}
-                  // input={input}
-                  field_name={field}
-                  onChange={handleChange}
-                  label_classname=""
-                  editing={editing}
-                />
-              );
-            })}
-          </div>
-
-          <TheCountryFields
+        <div className="flex w-full justify-end px-5">
+          <Edit
+            className={editing ? "h-6 w-6 text-accent" : "h-6 w-6"}
+            onClick={() => setEditing(!editing)}
+          />
+        </div>
+        <div
+          className="flex  justify-start items-center md:items-start h-full w-full flex-col gap-10
+       rounded-lg bg-secondary/5 p-2 md:flex-row"
+        >
+          {/* avatar */}
+          <ThePicUrlInput
+            container_classname="md:w-fit"
+            img_url={input.avatar}
             editing={editing}
-            country={{
-              city: input.city,
-              country: input.country,
-              phone: input.phone,
-            }}
-            setInput={(value) =>
+            setInputImage={(url) =>
               setInput((prev) => {
                 return {
                   ...prev,
-                  country: value.country,
-                  phone: value.phone,
-                  city: value.city,
+                  image_url: url ?? "",
                 };
               })
             }
           />
 
-          <TheStringListInput
-            editing={editing}
-            field_name="Skills"
-            field_key="skills"
-            input={input}
-            setInput={setInput}
-          />
-
-          {editing && (
-            <div className="flex w-full items-center justify-center">
-            <Button
-                type="submit"
-                className="btn btn-sm btn-outline min-w-[50%]"
-                variant={"ghost"}
-                size={"sm"}
-              >
-                {mutation.isLoading ? (
-                  <Loader className="h-6 w-6 animate-spin" />
-                ) : (
-                  <div></div>
-                )}
-                Save
-              </Button>
+          <div className="flex h-full w-full flex-col items-center gap-5">
+            {/* text fields */}
+            <div className="flex w-full flex-col items-center justify-center gap-2 lg:flex-row">
+              {text_fields.map((field) => {
+                return (
+                  <TheTextInput
+                    key={field}
+                    field_key={field}
+                    value={input[field]}
+                    // input={input}
+                    field_name={field}
+                    className="input input-bordered input-sm w-full  "
+                    label_classname="font-thin"
+                    onChange={handleChange}
+                    editing={editing}
+                  />
+                );
+              })}
             </div>
-          )}
+            {/* text area fields */}
+            <div className="flex w-full flex-col items-center justify-center gap-1 ">
+              {text_area_fields.map((field) => {
+                return (
+                  <TheTextAreaInput
+                    field_key={field}
+                    key={field}
+                    value={input[field]}
+                    // input={input}
+                    field_name={field}
+                    onChange={handleChange}
+                    label_classname=""
+                    editing={editing}
+                  />
+                );
+              })}
+            </div>
+
+            <TheCountryFields
+              editing={editing}
+              country={{
+                city: input.city,
+                country: input.country,
+                phone: input.phone,
+              }}
+              setInput={(value) =>
+                setInput((prev) => {
+                  return {
+                    ...prev,
+                    country: value.country,
+                    phone: value.phone,
+                    city: value.city,
+                  };
+                })
+              }
+            />
+
+            <TheStringListInput
+              editing={editing}
+              field_name="Skills"
+              field_key="skills"
+              input={input}
+              setInput={setInput}
+            />
+
+            {editing && (
+              <div className="flex w-full items-center justify-center">
+                <Button
+                  type="submit"
+                  className="btn btn-sm btn-outline min-w-[50%]"
+                  variant={"ghost"}
+                  size={"sm"}
+                >
+                  {mutation.isLoading ? (
+                    <Loader className="h-6 w-6 animate-spin" />
+                  ) : (
+                    <div></div>
+                  )}
+                  Save
+                </Button>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-    </form>
-  </div>
-);
+      </form>
+    </div>
+  );
 }
