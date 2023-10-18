024958b7b5402bc5dbde11a7168e8620d17bf259
Dennis kinuthia <denniskinuthiawaweru@gmail.com>
2023-10-18 14:21:43 +0300

    clena up

--- src/routes/about/index.page.tsx
+++
@@ -1,8 +0,0 @@
-import { PageProps } from "rakkasjs"
-import { HeroSection } from "./HeroSection"
-export default function Page({}:PageProps) {
-return (
-<div className="w-full h-full flex items-center justify-center">
-<HeroSection/>
-</div>
-)}
